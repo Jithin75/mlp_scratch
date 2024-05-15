03812d08ed6f4497fb97d084dc8e657a670d69f5
Jithin75 <jithuk02@gmail.com>
2024-05-15 00:32:54 -0400

    Implementation of Back Propogation

--- include/NeuralNetwork.hpp
+++ include/NeuralNetwork.hpp
@@ -15,11 +15,20 @@ class NeuralNetwork
         // Set Initial Input to NN
         void setInitialInput(std::vector<double> input);
 
+        // Set Target Output of NN
+        void setTargetOutput(std::vector<double> target);
+
         // Feed Forward Implementation
         void feedForward();
 
+        // Back Propogation Implementation
+        void backPropogation();
+
         // Visualise Neural Network Topology
         void prettyPrintNetwork();
+
+        // Expected that target is set, and calculates the errors
+        void setErrors();
 
         // Get Functions
         Matrix *getNeuronMatrix(int index) { return this->layers.at(index)->matrixify(0); }
@@ -27,12 +36,19 @@ class NeuralNetwork
         Matrix *getDerivedNeuronMatrix(int index) { return this->layers.at(index)->matrixify(2); }
         Matrix *getWeightMatrix(int index) { return new Matrix(*this->weightMatrices.at(index)); };
         void setNeuronValue(int indexLayer, int indexNeuron, double val) { this->layers.at(indexLayer)->setVal(indexNeuron, val); }
+        double getTotalError() {return this->error;}
+        std::vector<double> getErrors() {return this->errors;}
     private:
         int topologySize;
+        double error;
         std::vector<int> topology;
         std::vector<double> input;
+        std::vector<double> target;
+        std::vector<double> errors;
+        std::vector<double> historicalErrors;
         std::vector<Layer *> layers;
         std::vector<Matrix *> weightMatrices;
+        std::vector<Matrix *> gradientMatrices;
 };
 
 #endif //NEURALNETWORK_HPP
--- src/NeuralNetwork.cpp
+++ src/NeuralNetwork.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
+#include <cmath>
 #include "../include/NeuralNetwork.hpp"
 
 //Constructor
@@ -23,6 +25,12 @@ void NeuralNetwork::setInitialInput(std::vector<double> input) {
     this->input = input;
 
     this->layers.at(0)->layerCopy(input);
+}
+
+// Set Target Output of NN
+void NeuralNetwork::setTargetOutput(std::vector<double> target) {
+    this->target = target;
+    this->errors = std::vector<double> (target.size(), 0);
 }
 
 // Visualise Neural Network Topology
@@ -61,5 +69,102 @@ void NeuralNetwork::feedForward() {
         delete a;
         delete b;
         delete c;
+    }
+}
+
+// Expected that target is set, and calculates the errors
+void NeuralNetwork::setErrors() {
+    if (this->target.size() == 0) {
+        std::cerr << "No target set for this Neural Network" << std::endl;
+        assert(false);
+    }
+
+    if (this->target.size() != this->layers.at(this->layers.size() - 1)->getSize()) {
+        std::cerr << "Target size do not match the last layer of the Neural Network topology" << std::endl;
+        assert(false);
+    }
+
+    this->error = 0.00;
+    std::vector<Neuron *> outputNeurons = this->layers.at(this->layers.size() - 1)->getNeurons();
+    for(int i = 0; i < this->target.size(); i++) {
+        double temp = pow((outputNeurons.at(i)->getActivatedVal() - target.at(i)), 2);
+        this->errors.at(i) = temp;
+        this->error += temp;
+    }
+    this->historicalErrors.push_back(this->error);
+}
+
+// Back Propogation Implementation
+void NeuralNetwork::backPropogation() {
+    std::vector<Matrix *> newWeights;
+    Matrix *gradient;
+
+    // Final Output layer to the last hidden layer
+    int ouputLayerIndex = this->layers.size() - 1;
+    Matrix *derrivedOutputLayer = this->layers.at(ouputLayerIndex)->matrixify(2);
+    Matrix *gradientOutputLayer = new Matrix(1, this->layers.at(ouputLayerIndex)->getSize(), false);
+    for(int i = 0; i < this->errors.size(); i++) {
+        gradientOutputLayer->setVal(0,i,(derrivedOutputLayer->getVal(0,i) * this->errors.at(i)));
+    }
+
+    int lastHiddenLayerIndex = ouputLayerIndex - 1;
+    Layer *lastHiddenLayer = this->layers.at(lastHiddenLayerIndex);
+    Matrix *weightsBeforeOutput = this->weightMatrices.at(lastHiddenLayerIndex);
+    Matrix *deltaBeforeOutput = (*lastHiddenLayer->matrixify(1)->transpose()) * (*gradientOutputLayer); 
+    
+    Matrix *newBeforeOutput = new Matrix(weightsBeforeOutput->getRows(), weightsBeforeOutput->getCols(), false);
+    for(int i = 0; i < weightsBeforeOutput->getRows(); i++) {
+        for(int j = 0; j < weightsBeforeOutput->getCols(); j++) {
+            newBeforeOutput->setVal(i, j, weightsBeforeOutput->getVal(i,j) - deltaBeforeOutput->getVal(i,j));
+        }
+    }
+
+    newWeights.push_back(newBeforeOutput);
+    gradient = new Matrix(gradientOutputLayer->getRows(),gradientOutputLayer->getCols(),false);
+    for(int i = 0; i < gradientOutputLayer->getRows(); i++) {
+        for(int j = 0; j < gradientOutputLayer->getCols(); j++) {
+            gradient->setVal(i, j, gradientOutputLayer->getVal(i,j));
+        }
+    }
+
+    // Moving backwards from the last hidden layer to the input layer
+    for(int i = lastHiddenLayerIndex; i > 0; i--) {
+        Layer *curLayer = this->layers.at(i);
+        Matrix *deriveCurLayer = curLayer->matrixify(2);
+        Matrix *activateCurLayer = curLayer->matrixify(1);
+        Matrix *derivedGradients = new Matrix(1, curLayer->getSize(), false);
+
+        Matrix *rightWeightMatrix = this->weightMatrices.at(i);
+        Matrix *leftWeightMatrix = this->weightMatrices.at(i - 1);
+
+        for(int i = 0; i < rightWeightMatrix->getRows(); i++) {
+            double sum = 0;
+            for(int j = 0; j < rightWeightMatrix->getCols(); j++) {
+                double p = gradient->getVal(0,j) * rightWeightMatrix->getVal(i,j);
+                sum += p;
+            }
+            derivedGradients->setVal(0, i, sum * activateCurLayer->getVal(0,i));
+        }
+
+        Matrix *leftNeurons = (i - 1) == 0 ? this->layers.at(0)->matrixify(0) : this->layers.at(i - 1)->matrixify(1);
+        Matrix *deltaWeights = (*leftNeurons->transpose()) * (*derivedGradients); 
+        Matrix *newCurWeights = new Matrix(deltaWeights->getRows(), deltaWeights->getCols(), false);
+        for(int i = 0; i < deltaWeights->getRows(); i++) {
+            for(int j = 0; j < deltaWeights->getCols(); j++) {
+                newCurWeights->setVal(i, j, leftWeightMatrix->getVal(i,j) - deltaWeights->getVal(i,j));
+            }
+        }
+
+        gradient = new Matrix(derivedGradients->getRows(),derivedGradients->getCols(),false);
+        for(int i = 0; i < derivedGradients->getRows(); i++) {
+            for(int j = 0; j < derivedGradients->getCols(); j++) {
+                gradient->setVal(i, j, derivedGradients->getVal(i,j));
+            }
+        }
+        newWeights.push_back(newCurWeights);
+    }
+
+    for (int i = newWeights.size() - 1; i >= 0; i--) {
+        this->weightMatrices[newWeights.size() - 1 - i] = newWeights[i];
     }
 }
